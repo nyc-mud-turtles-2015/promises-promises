a7a94ff0bb2ad1cc86c5665d07d83dfa4a2c903c
Steven Cassidy <steven@mumsnet.com>
2015-10-28 15:27:57 -0400

    Add a second example with Promise.all

--- public/app.js
+++ public/app.js
@@ -19,16 +19,29 @@ function simpleAjax(method, url) {
   return new Promise(executor);
 }
 
-//We can make a get method using bind.
+//We can make a get function using bind to fix the first param of simpleAjax.
 var ajaxGet = simpleAjax.bind(null, 'GET');
 
 document.addEventListener('DOMContentLoaded', function(){
+
+  //Make a name
   ajaxGet('/word/n')
-  .then(function(arg){
+  .then(function(serverResponse){
+    // serverResponse because that's how we resolved the promise above    
     return arg + ' H. ' ;
   }).then(function(arg){
+    // the arg here is the return value of the prior function in the then 
+    // chain. This time round we'll return a promise of a value
     return simpleAjax('get', '/word/y').then(function(response){ return arg + response; });
   }).then(function(arg){
-    document.getElementById('output').innerHTML = 'Hello ' + arg;
+    document.getElementById('output1').innerHTML = 'Hello ' + arg;
   });
+
+  // This time we'll use Promise.all for added ease
+  var p1 = ajaxGet('/word/n');
+  var p2 = ajaxGet('/word/n');
+  Promise.all([p1, p2]).then(function(values){
+    document.getElementById('output2').innerHTML = 'Goodbye ' + values[0] + 'G. ' + values[1];
+  })
+
 });
